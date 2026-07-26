f9991fa127912f3e1ab1da49c37b02b621814b7f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-26 23:37:07 +0530

    Added solution - Time: O(n log n), Space: O(n)

---
+++ 702849-complete-binary-tree-traversal-with-array-input-GFG/702849-complete-binary-tree-traversal-with-array-input.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelSort(int[] arr) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int n = arr.length;
+        int index = 0;
+        int levelSize = 1;
+
+        while (index < n) {
+            ArrayList<Integer> level = new ArrayList<>();
+            int count = Math.min(levelSize, n - index);
+
+            for (int i = 0; i < count; i++) {
+                level.add(arr[index++]);
+            }
+
+            Collections.sort(level);
+            ans.add(level);
+            levelSize *= 2;
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
