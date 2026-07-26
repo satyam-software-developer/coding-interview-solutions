79a54db52447d445438c96b366e0984128a347c5
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-26 13:52:25 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878198-method-overloading-in-java-GFG/878198-method-overloading-in-java.java
@@ -0,0 +1,20 @@
+//  Implement Calculator class with all methods
+
+class Calculator{
+    
+    int sum(int a, int b){
+        return a + b;
+    }
+    
+    int sum(int a, int b, int c){
+        return a + b + c;
+    }
+    
+    double sum(double x, double y){
+        return x + y;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
