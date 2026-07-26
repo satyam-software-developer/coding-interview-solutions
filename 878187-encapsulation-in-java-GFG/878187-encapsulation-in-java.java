41c1c19d29987ffaa5be55f849a5a8d5e7f453d3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-26 14:12:44 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878187-encapsulation-in-java-GFG/878187-encapsulation-in-java.java
@@ -0,0 +1,31 @@
+//  Write the complete Person class here
+class Person{
+    
+    private String name;
+    private int age;
+    
+    Person(){
+        name = "Geeks";
+        age = 10;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
