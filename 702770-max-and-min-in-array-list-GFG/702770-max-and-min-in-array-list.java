4d02efe46071ebbd429ae5689095a7e086a04796
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-28 12:53:29 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702770-max-and-min-in-array-list-GFG/702770-max-and-min-in-array-list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int maximumElement(ArrayList<Integer> arr) {
+        // code here
+        return Collections.max(arr);
+    }
+
+    public static int minimumElement(ArrayList<Integer> arr) {
+        // code here.
+        return Collections.min(arr);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
