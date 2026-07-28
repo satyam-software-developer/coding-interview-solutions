5656b8cfbeee04b07e136f8413b5fa79483ab8fb
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-28 12:06:50 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 702909-linked-list-insertion-GFG/702909-linked-list-insertion.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static LinkedList<Integer> insertion(int arr[]) {
+        // Your code here
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        for(int num : arr){
+            list.add(num);
+        }
+        
+        return list;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
