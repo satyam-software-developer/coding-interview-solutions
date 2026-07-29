8482097110695c489e91abfca54410c231160275
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-29 23:37:49 +0530

    Added solution - Time: O(n log n), Space: O(log n)

---
+++ 878166-split-array-into-minimum-subsets-GFG/878166-split-array-into-minimum-subsets.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    int minSubsets(int arr[]) {
+        Arrays.sort(arr);
+
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1] + 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
