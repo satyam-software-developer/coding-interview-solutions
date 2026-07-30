51e648cf9cf41207a9e90efb0f1e044dcb8a3c02
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-30 23:47:13 +0530

    Time: 0 ms (100%), Space: 43 MB (79.43%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int pushes = 0;
+
+        for (int i = 0; i < n; i++) {
+            pushes += i / 8 + 1;
+        }
+
+        return pushes;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
