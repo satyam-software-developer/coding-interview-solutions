9752f467553807a3fcbaad5aba8a124dc1996fe3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-01 13:46:21 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 702858-count-only-once-GFG/702858-count-only-once.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int countOnce(int arr[]) {
+        // Your code here
+        HashSet<Integer> set =  new HashSet<>();
+        
+        for(int num : arr){
+            
+            set.add(num);
+        }
+        
+        return set.size();
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
