692ae3688c8b601c0d2fd0fae5c8b08b5c889037
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-01 23:43:34 +0530

    Added solution - Time: O(n + m), Space: O(n)

---
+++ 702954-max-after-m-range-increments-GFG/702954-max-after-m-range-increments.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMax(int n, int[] a, int[] b, int[] k) {
+        long[] diff = new long[n + 1];
+
+        for (int i = 0; i < a.length; i++) {
+            diff[a[i]] += k[i];
+            if (b[i] + 1 < n) {
+                diff[b[i] + 1] -= k[i];
+            }
+        }
+
+        long max = 0;
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        return (int) max;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
