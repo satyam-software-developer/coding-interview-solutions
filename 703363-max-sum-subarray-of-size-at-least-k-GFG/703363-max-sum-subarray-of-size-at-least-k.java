651040914e975f8247ec197205550b26022438a0
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-03 23:44:43 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703363-max-sum-subarray-of-size-at-least-k-GFG/703363-max-sum-subarray-of-size-at-least-k.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxSumWithK(int[] arr, int k) {
+        int n = arr.length;
+
+        int[] maxEndHere = new int[n];
+        maxEndHere[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            maxEndHere[i] = Math.max(arr[i], maxEndHere[i - 1] + arr[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int ans = sum;
+
+        for (int i = k; i < n; i++) {
+            sum += arr[i] - arr[i - k];
+            ans = Math.max(ans, sum);
+            ans = Math.max(ans, sum + maxEndHere[i - k]);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
