efe8cf810d645a6ae462609a0c8a00ca36e7af8a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-05 00:07:02 +0530

    Time: 3 ms (81.76%), Space: 46.8 MB (53.61%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            set.add(num);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
