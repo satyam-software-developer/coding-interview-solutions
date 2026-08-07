20e350f4075b1f37956caf97b6d551ed297e381e
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-07 23:52:08 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703687-friends-pairing-problem-GFG/703687-friends-pairing-problem.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        if (n <= 2) return n;
+        
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + (long) (i - 1) * dp[i - 2];
+        }
+        
+        return (int) dp[n];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
