14e1ac1d7939fd8ed7144f44817417c1ac16d44a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-09 01:11:55 +0530

    Time: 33 ms (81.58%), Space: 137.3 MB (57.89%) - LeetHub - Time: O(n + m), Space: O(m)

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] last = new int[m];
+        java.util.Arrays.fill(last, -1);
+
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                last[j] = i;
+                j--;
+            }
+            i--;
+        }
+
+        int[] ans = new int[m];
+        boolean usedMismatch = false;
+
+        i = 0;
+        j = 0;
+
+        while (i < n && j < m) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j] = i;
+                j++;
+            } else if (!usedMismatch &&
+                       (j == m - 1 || i < last[j + 1])) {
+                ans[j] = i;
+                j++;
+                usedMismatch = true;
+            }
+
+            i++;
+        }
+
+        if (j < m) {
+            return new int[0];
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
