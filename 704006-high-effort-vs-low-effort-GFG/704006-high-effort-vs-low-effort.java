5d4b4f4db7c90e2d8c42ce363c279f660fe0b69d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-10 23:47:15 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704006-high-effort-vs-low-effort-GFG/704006-high-effort-vs-low-effort.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxTask(int[] h, int[] l) {
+        int noTask = 0;
+        int task = 0;
+
+        for (int i = 0; i < h.length; i++) {
+            int newNoTask = Math.max(noTask, task);
+            int newTask = Math.max(noTask + h[i], Math.max(noTask, task) + l[i]);
+
+            noTask = newNoTask;
+            task = newTask;
+        }
+
+        return Math.max(noTask, task);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
