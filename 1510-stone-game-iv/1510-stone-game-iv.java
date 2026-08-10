33ffca6397bc05bed54ae2249482d2a93ac4ffba
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-10 23:54:34 +0530

    Time: 15 ms (64.49%), Space: 42.5 MB (68.41%) - LeetHub - Time: O(n * sqrt(n)), Space: O(n)

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
