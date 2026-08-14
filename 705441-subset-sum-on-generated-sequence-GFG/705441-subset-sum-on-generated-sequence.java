1c3dbaa15346e90372d4502823c511cd7f8a9a2f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-14 23:57:30 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 705441-subset-sum-on-generated-sequence-GFG/705441-subset-sum-on-generated-sequence.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPossible(int[] arr, int s, int x) {
+        long current = s;
+        long total = s;
+        long[] nums = new long[arr.length + 1];
+        nums[0] = s;
+
+        int count = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            current = total + arr[i];
+            total += current;
+
+            if (current <= x) {
+                nums[count++] = current;
+            }
+        }
+
+        long remaining = x;
+
+        for (int i = count - 1; i >= 0; i--) {
+            if (nums[i] <= remaining) {
+                remaining -= nums[i];
+            }
+        }
+
+        return remaining == 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
