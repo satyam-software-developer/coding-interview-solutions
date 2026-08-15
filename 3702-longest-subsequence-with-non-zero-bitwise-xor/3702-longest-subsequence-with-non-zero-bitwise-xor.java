8dc9216fa9a977c059e8c9e77157d154a4d71b76
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-15 23:48:53 +0530

    Time: 1 ms (100%), Space: 133.3 MB (88.24%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        if (xor != 0) {
+            return nums.length;
+        }
+
+        for (int num : nums) {
+            if (num != 0) {
+                return nums.length - 1;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
