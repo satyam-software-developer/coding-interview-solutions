29c985f8610ab0423f7685645e2fbe8dd542cead
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-16 23:02:49 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702739-min-product-subset-GFG/702739-min-product-subset.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minProd(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int x = arr[i];
+
+            int a = min * x;
+            int b = max * x;
+
+            int newMin = Math.min(x, Math.min(a, b));
+            int newMax = Math.max(x, Math.max(a, b));
+
+            min = Math.min(min, newMin);
+            max = Math.max(max, newMax);
+        }
+
+        return min;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
