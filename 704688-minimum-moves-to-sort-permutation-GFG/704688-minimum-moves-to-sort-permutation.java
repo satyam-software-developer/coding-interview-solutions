176974537f63c20adcd3fccb53c6bf9a1649c22b
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-25 23:36:39 +0530

    Added solution

---
+++ 704688-minimum-moves-to-sort-permutation-GFG/704688-minimum-moves-to-sort-permutation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minMoves(int[] arr) {
+        int n = arr.length;
+        int[] pos = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            pos[arr[i]] = i;
+        }
+
+        int maxLen = 1;
+        int len = 1;
+
+        for (int value = 2; value <= n; value++) {
+            if (pos[value] > pos[value - 1]) {
+                len++;
+            } else {
+                len = 1;
+            }
+            maxLen = Math.max(maxLen, len);
+        }
+
+        return n - maxLen;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
