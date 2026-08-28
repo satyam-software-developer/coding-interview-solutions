a80aa3525427a2428af72e2a9e0cbc7b7565e233
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-08-28 23:31:49 +0530

    Added solution - Time: O(mat.length), Space: O(1)

---
+++ 705346-minimum-cost-selection-GFG/705346-minimum-cost-selection.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minCost(int[][] mat) {
+        int a = mat[0][0];
+        int b = mat[0][1];
+        int c = mat[0][2];
+
+        for (int i = 1; i < mat.length; i++) {
+            int na = mat[i][0] + Math.min(b, c);
+            int nb = mat[i][1] + Math.min(a, c);
+            int nc = mat[i][2] + Math.min(a, b);
+
+            a = na;
+            b = nb;
+            c = nc;
+        }
+
+        return Math.min(a, Math.min(b, c));
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
