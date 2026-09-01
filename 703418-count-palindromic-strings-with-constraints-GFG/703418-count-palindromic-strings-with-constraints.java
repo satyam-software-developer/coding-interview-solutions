28df921e094934bb128ca1ce2fb64ab0c103c384
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-01 23:55:59 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 703418-count-palindromic-strings-with-constraints-GFG/703418-count-palindromic-strings-with-constraints.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int palindromicStrings(int n, int k) {
+        long MOD = 1000000007L;
+        long ans = 0;
+
+        for (int len = 1; len <= n; len++) {
+            int half = len / 2;
+            long ways = 1;
+
+            for (int j = 0; j < half; j++) {
+                ways = ways * (k - j) % MOD;
+            }
+
+            if (len % 2 == 0) {
+                ans = (ans + ways) % MOD;
+            } else {
+                long oddWays = ways * (k - half) % MOD;
+                ans = (ans + oddWays) % MOD;
+            }
+        }
+
+        return (int) ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
