1ed00ca322f0e0eec4d85a6083aaf3965ea05abe
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-03 00:05:34 +0530

    Time: 1 ms (31.89%), Space: 45.1 MB (73.43%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        boolean hasOdd = false;
+        boolean hasEven = false;
+
+        for (int num : nums1) {
+            if (num % 2 == 0) {
+                hasEven = true;
+            } else {
+                hasOdd = true;
+            }
+        }
+
+        if (!hasOdd || !hasEven) {
+            return true;
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
