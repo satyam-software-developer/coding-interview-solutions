b7dc8e8e524825ec29e1896594688631449c1d20
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-04 23:51:10 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 713134-bird-and-max-fruit-gathering-GFG/713134-bird-and-max-fruit-gathering.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxFruits(ArrayList<Integer> arr, int m) {
+        int n = arr.size();
+
+        if (m >= n) {
+            int total = 0;
+            for (int x : arr) {
+                total += x;
+            }
+            return total;
+        }
+
+        int sum = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < m; i++) {
+            sum += arr.get(i);
+        }
+
+        maxSum = sum;
+
+        for (int i = m; i < n + m - 1; i++) {
+            sum += arr.get(i % n);
+            sum -= arr.get((i - m) % n);
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
