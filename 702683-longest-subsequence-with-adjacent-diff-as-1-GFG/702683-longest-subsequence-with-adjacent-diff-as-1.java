d4f29a62dd9bb7f45f9401e33b0ccffcab0e3eca
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-05 23:50:31 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702683-longest-subsequence-with-adjacent-diff-as-1-GFG/702683-longest-subsequence-with-adjacent-diff-as-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestSubseq(int[] arr) {
+        int[] dp = new int[1000002];
+        int ans = 1;
+
+        for (int num : arr) {
+            dp[num] = Math.max(dp[num - 1], dp[num + 1]) + 1;
+            ans = Math.max(ans, dp[num]);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
