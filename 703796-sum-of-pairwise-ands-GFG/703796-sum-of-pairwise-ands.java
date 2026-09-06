a0b9ab7372ff96f7042c35e8613d87999d0ab3fe
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-06 23:10:59 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703796-sum-of-pairwise-ands-GFG/703796-sum-of-pairwise-ands.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long pairAndSum(int[] arr) {
+        long ans = 0;
+
+        for (int bit = 0; bit < 31; bit++) {
+            long count = 0;
+
+            for (int num : arr) {
+                if ((num & (1 << bit)) != 0) {
+                    count++;
+                }
+            }
+
+            ans += count * (count - 1) / 2 * (1L << bit);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
