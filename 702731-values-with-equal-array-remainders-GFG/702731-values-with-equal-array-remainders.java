4b24106f2305216ca5a748e43b0c1d0378ce321c
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-11 22:52:21 +0530

    Added solution - Time: O(n log M + sqrt(M)), Space: O(log M)

---
+++ 702731-values-with-equal-array-remainders-GFG/702731-values-with-equal-array-remainders.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int sameMod(int[] arr) {
+        int gcd = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            gcd = gcd(gcd, Math.abs(arr[i] - arr[0]));
+        }
+
+        if (gcd == 0) {
+            return -1;
+        }
+
+        int count = 0;
+
+        for (int i = 1; i * i <= gcd; i++) {
+            if (gcd % i == 0) {
+                count++;
+                if (i != gcd / i) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
