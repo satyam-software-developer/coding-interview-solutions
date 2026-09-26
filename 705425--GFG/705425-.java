1ba7d27a839a9ed1b54ee4c4a4cc9cfc3d2ad553
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-26 23:52:43 +0530

    Added solution - Time: O((x/s) * (x/m)), Space: O(1)

---
+++ 705425--GFG/705425-.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumCost(int x, int s, int m, int l, int cs, int cm, int cl) {
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= x / s + 1; i++) {
+            for (int j = 0; j <= x / m + 1; j++) {
+                int area = i * s + j * m;
+                if (area >= x) {
+                    ans = Math.min(ans, i * cs + j * cm);
+                } else {
+                    int k = (x - area + l - 1) / l;
+                    ans = Math.min(ans, i * cs + j * cm + k * cl);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
