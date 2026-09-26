d6742163027be07fcae50d23b758d86de5daf1d3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-09-27 00:03:45 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702968-fill-method-GFG/702968-fill-method.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void fillArray(int arr[], int k) {
+        // code here
+         java.util.Arrays.fill(arr, k);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
